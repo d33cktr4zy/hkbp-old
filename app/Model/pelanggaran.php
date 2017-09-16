968bcd2450e7d06fad68d5fc78b97c1a3c672cb7
d33cktr4zy <gabriel.fermy@gmail.com>
2017-09-17 06:46:22 +0700

    revisi 1: penambahan table pelanggaran

---
+++ app/Model/pelanggaran.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace hkbp\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class pelanggaran extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'tbtr_pelanggaran';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+//    protected $fillable = ['name', 'email', 'password'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+//    protected $hidden = ['password', 'remember_token'];
+//
+}
